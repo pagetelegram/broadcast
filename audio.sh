bba67b16da77c91c0a3eb43f0c1173c81d933b5a
Page Telegram <43476661+pagetelegram@users.noreply.github.com>
2020-07-01 20:56:31 -0500

    ver 2
    normalize audio and insert radio jingle every 4 songs

--- audio.sh
+++ audio.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-cd ~
+# ver 2.0
 mkdir m
 sudo mount /dev/sda1 ~/m
-for (( ; ; ))
+pico2wave -w ~/r.wav "Welcome to my radio station..."
+sox -v .3 ~/r.wav radio.wav
+while :
 do
 cd ~/m
 find "$PWD" | grep .mp3 > ~/audio.pls
 find "$PWD" | grep .m4a >> ~/audio.pls
 find "$PWD" | grep .ogg >> ~/audio.pls
-mpv --shuffle --playlist ~/audio.pls
+cat ~/audio.pls | shuf > ~/ply.pls
+awk '{print}; NR % 4 == 0 {print "~/radio.wav"}' ~ply.pls > audio.pls
+mpv --af=lavfi=dynaudnorm --playlist ~/audio.pls
 done
+
+

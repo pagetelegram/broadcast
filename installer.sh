2bb4dadcfc0ca7cd706783d343f69d801d386714
Page Telegram <43476661+pagetelegram@users.noreply.github.com>
2020-07-03 19:10:30 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,4 @@
+sudo apt install sox mpv
+wget http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico0_1.0+git20130326-9_armhf.deb
+wget http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico-utils_1.0+git20130326-9_armhf.deb
+sudo apt-get install -f ./libttspico0_1.0+git20130326-9_armhf.deb ./libttspico-utils_1.0+git20130326-9_armhf.deb
